2b8dd592a3a8c35001f30937077c85cd1b796cc0
Alex <84599917+GyverLibs@users.noreply.github.com>
2021-12-21 18:59:45 +0300

    upd

--- src/INA219.h
+++ src/INA219.h
@@ -53,116 +53,132 @@
 
 class INA219 {
 public:
+    // конструктор
     INA219(const float r_shunt = 0.1f, const float i_max = 3.2f, const uint8_t address = 0x40)
     : _r_shunt(r_shunt), _i_max(i_max), _iic_address(address) {}
-
+    
+    // конструктор
     INA219(const uint8_t address)
     : _r_shunt(0.1f), _i_max(3.2f), _iic_address(address) {}
-
-    bool begin(void) {						// Инициализация и проверка
-        Wire.begin();							// Инициализация шины I2c
-        if (!testConnection()) return false;  // Проверка присутствия
-        calibrate();							// Рассчет калибровочного значения и инициализация
-        return true;							// Вернуть true если все ок
+    
+    // Инициализация и проверка
+    bool begin(void) {
+        Wire.begin();                           // Инициализация шины I2c
+        if (!testConnection()) return false;    // Проверка присутствия
+        calibrate();                            // расчёт калибровочного значения и инициализация
+        return true;                            // Вернуть true если все ок
     }
-
-    void sleep(bool state) {														// Установка / снятие режима сна
+    
+    // Установка / снятие режима сна
+    void sleep(bool state) {
         uint16_t cfg_register = readRegister(INA219_CFG_REG_ADDR) & ~(0b111);			// Прочитать конф. регистр и стереть биты режима
         writeRegister(INA219_CFG_REG_ADDR, cfg_register | (state ? 0b000 : 0b111));	// Записать новое значение конф. регистра с выбранным режимом
     }
-
-    void adjCalibration(int16_t adj) {			// Подстройка калибровочного значения
-        setCalibration(getCalibration() + adj);	// Читаем и модифицируем значение
-        _cal_value = _cal_value + adj;			// Обновляем внутреннюю переменную
+    
+    // Подстройка калибровочного значения
+    void adjCalibration(int16_t adj) {
+        setCalibration(getCalibration() + adj);     // Читаем и модифицируем значение
+        _cal_value = _cal_value + adj;              // Обновляем внутреннюю переменную
     }	
     
-    void setCalibration(uint16_t cal) {			// Установка калибровочного значения 
-        writeRegister(INA219_CAL_REG_ADDR, cal);	// Пишем значение в регистр калибровки
-        _cal_value = cal;							// Обновляем внутреннюю переменную 
+    // Установка калибровочного значения 
+    void setCalibration(uint16_t cal) {
+        writeRegister(INA219_CAL_REG_ADDR, cal);    // Пишем значение в регистр калибровки
+        _cal_value = cal;                           // Обновляем внутреннюю переменную 
     }
-
-    uint16_t getCalibration(void) {						// Чтение калибровочного значения 
-        _cal_value = readRegister(INA219_CAL_REG_ADDR); 	// Обновляем внутреннюю переменну
-        return _cal_value;								// Возвращаем значение
+    
+    // Чтение калибровочного значения 
+    uint16_t getCalibration(void) {
+        _cal_value = readRegister(INA219_CAL_REG_ADDR); // Обновляем внутреннюю переменну
+        return _cal_value;                              // Возвращаем значение
     }
-
-    void setResolution(bool ch, uint8_t mode) {						// Установка разрешения для выбранного канала
-        uint16_t cfg_register = readRegister(INA219_CFG_REG_ADDR);	// Читаем конф. регистр
-        cfg_register &= ~ ((0b1111) << (ch ? 7 : 3));					// Сбрасываем нужную пачку бит, в зависимости от канала
-        cfg_register |= mode << (ch ? 7 : 3);							// Пишем нужную пачку бит, в зависимости от канала
-        writeRegister(INA219_CFG_REG_ADDR, cfg_register);				// Пишем новое значение конф. регистра
+    
+    // Установка разрешения для выбранного канала
+    void setResolution(bool ch, uint8_t mode) {
+        uint16_t cfg_register = readRegister(INA219_CFG_REG_ADDR);      // Читаем конф. регистр
+        cfg_register &= ~ ((0b1111) << (ch ? 7 : 3));                   // Сбрасываем нужную пачку бит, в зависимости от канала
+        cfg_register |= mode << (ch ? 7 : 3);                           // Пишем нужную пачку бит, в зависимости от канала
+        writeRegister(INA219_CFG_REG_ADDR, cfg_register);               // Пишем новое значение конф. регистра
     }
-
-    float getShuntVoltage(void) {							// Чтение напряжения на шунте
-        setCalibration(_cal_value);							// Принудительное обновление калибровки (на случай внезапного ребута INA219)
-        int16_t value = readRegister(INA219_SHUNT_REG_ADDR);  // Чтение регистра напряжения шунта
-        return value * 0.00001f;								// LSB = 10uV = 0.00001V, умножаем и возвращаем
+    
+    // Чтение напряжения на шунте
+    float getShuntVoltage(void) {
+        setCalibration(_cal_value);                             // Принудительное обновление калибровки (на случай внезапного ребута INA219)
+        int16_t value = readRegister(INA219_SHUNT_REG_ADDR);    // Чтение регистра напряжения шунта
+        return value * 0.00001f;                                // LSB = 10uV = 0.00001V, умножаем и возвращаем
     }
-
-    float getVoltage(void) {								// Чтение напряжения 
-        uint16_t value = readRegister(INA219_VBUS_REG_ADDR);	// Чтение регистра напряжения 
-        return (value >> 3) * 0.004f;							// LSB = 4mV = 0.004V, Сдвигаем значение до 12 бит и умножаем 
+    
+    // Чтение напряжения 
+    float getVoltage(void) {
+        uint16_t value = readRegister(INA219_VBUS_REG_ADDR);    // Чтение регистра напряжения 
+        return (value >> 3) * 0.004f;                           // LSB = 4mV = 0.004V, Сдвигаем значение до 12 бит и умножаем 
     }
-
-    float getCurrent(void) {								// Чтение тока
-        setCalibration(_cal_value);							// Принудительное обновление калибровки (на случай внезапного ребута INA219)
-        int16_t value = readRegister(INA219_CUR_REG_ADDR);    // Чтение регистра тока
-        return value * _current_lsb;							// LSB рассчитывается на основе макс. ожидаемого тока, умножаем и возвращаем
+    
+    // Чтение тока
+    float getCurrent(void) {
+        setCalibration(_cal_value);                             // Принудительное обновление калибровки (на случай внезапного ребута INA219)
+        int16_t value = readRegister(INA219_CUR_REG_ADDR);      // Чтение регистра тока
+        return value * _current_lsb;                            // LSB рассчитывается на основе макс. ожидаемого тока, умножаем и возвращаем
     }	
-
-    float getPower(void) {									// Чтение мощности 
-        setCalibration(_cal_value);							// Принудительное обновление калибровки (на случай внезапного ребута INA219)
-        uint16_t value = readRegister(INA219_POWER_REG_ADDR); // Чтение регистра мощности
-        return value * _power_lsb;							// LSB в 20 раз больше LSB для тока, умножаем возвращаем
+    
+    // Чтение мощности 
+    float getPower(void) {
+        setCalibration(_cal_value);                             // Принудительное обновление калибровки (на случай внезапного ребута INA219)
+        uint16_t value = readRegister(INA219_POWER_REG_ADDR);   // Чтение регистра мощности
+        return value * _power_lsb;                              // LSB в 20 раз больше LSB для тока, умножаем возвращаем
     }
 
 private:
-    const uint8_t _iic_address = 0x00;	// Адрес на шине I2c					
-    const float _r_shunt = 0.0;			// Сопротивление шунта	
-    const float _i_max = 0.0;			// Макс. ожидаемый ток
+    const uint8_t _iic_address = 0x00;  // Адрес на шине I2c					
+    const float _r_shunt = 0.0;         // Сопротивление шунта	
+    const float _i_max = 0.0;           // Макс. ожидаемый ток
 
-    float _current_lsb = 0.0;			// LSB для тока
-    float _power_lsb = 0.0;				// LSB для мощности
-    uint16_t _cal_value = 0;			// Калибровочное значение
-
-    void writeRegister(uint8_t address, uint16_t data) { 	// Запись 16-ти битного регистра INA219
-        Wire.beginTransmission(_iic_address);					// Начинаем передачу
-        Wire.write(address);									// Отправляем адрес
-        Wire.write(highByte(data));							// Отправляем старший байт
-        Wire.write(lowByte(data));							// Отправляем младший байт
-        Wire.endTransmission();								// Заканчиваем передачу
+    float _current_lsb = 0.0;           // LSB для тока
+    float _power_lsb = 0.0;             // LSB для мощности
+    uint16_t _cal_value = 0;            // Калибровочное значение
+    
+    // Запись 16-ти битного регистра INA219
+    void writeRegister(uint8_t address, uint16_t data) {
+        Wire.beginTransmission(_iic_address);               // Начинаем передачу
+        Wire.write(address);                                // Отправляем адрес
+        Wire.write(highByte(data));                         // Отправляем старший байт
+        Wire.write(lowByte(data));                          // Отправляем младший байт
+        Wire.endTransmission();                             // Заканчиваем передачу
     }
-
-    uint16_t readRegister(uint8_t address) {	// Чтение 16-ти битного регистра INA219
-        Wire.beginTransmission(_iic_address);		// Начинаем передачу
-        Wire.write(address);						// Отправляем адрес	
-        Wire.endTransmission();					// Заканчиваем передачу
-        Wire.requestFrom(_iic_address, 2);		// Запрашиваем 2 байта
-        return Wire.read() << 8 | Wire.read();	// Клеим и возвращаем результат
+    
+    // Чтение 16-ти битного регистра INA219
+    uint16_t readRegister(uint8_t address) {
+        Wire.beginTransmission(_iic_address);       // Начинаем передачу
+        Wire.write(address);                        // Отправляем адрес	
+        Wire.endTransmission();                     // Заканчиваем передачу
+        Wire.requestFrom(_iic_address, (uint8_t)2); // Запрашиваем 2 байта
+        return Wire.read() << 8 | Wire.read();      // Клеим и возвращаем результат
     }
-
-    bool testConnection(void) {					// Проверка присутствия
-        Wire.beginTransmission(_iic_address);		// Начинаем передачу 
-        return (bool)!Wire.endTransmission();		// Сразу заканчиваем, инвертируем результат
+    
+    // Проверка присутствия
+    bool testConnection(void) {
+        Wire.beginTransmission(_iic_address);       // Начинаем передачу 
+        return (bool)!Wire.endTransmission();       // Сразу заканчиваем, инвертируем результат
     }
+    
+    // Процедура рассчета калибровочного значения и инициализации
+    void calibrate(void) {
+        writeRegister(INA219_CFG_REG_ADDR, 0x8000);                 // Принудительный сброс
 
-    void calibrate(void) {															// Процедура рассчета калибровочного значения и инициализации
-        writeRegister(INA219_CFG_REG_ADDR, 0x8000);									// Принудительный сброс
+        _current_lsb = _i_max / 32768.0f;                           // Рассчет LSB для тока (см. доку INA219)
+        _power_lsb = _current_lsb * 20.0f;                          // Рассчет LSB для мощности (см. доку INA219)
+        _cal_value = trunc(0.04096f / (_current_lsb * _r_shunt));   // Рассчет калибровочного значения (см. доку INA219)
 
-        _current_lsb = _i_max / 32768.0f;												// Рассчет LSB для тока (см. доку INA219)
-        _power_lsb = _current_lsb * 20.0f;											// Рассчет LSB для мощности (см. доку INA219)
-        _cal_value = trunc(0.04096f / (_current_lsb * _r_shunt));						// Рассчет калибровочного значения (см. доку INA219)
+        setCalibration(_cal_value);                                 // Записываем стандартное калибровочное значение
 
-        setCalibration(_cal_value);													// Записываем стандартное калибровочное значение
+        uint16_t cfg_register = readRegister(INA219_CFG_REG_ADDR) & ~(0b11 << 11);  // Читаем регистр калибровки, сбрасываем биты диапазона напряжения шунта
+        uint16_t vshunt_max_mv = (_r_shunt * _i_max) * 1000;        // Считаем макс напряжение на шунте при макс ожидаемом токе
 
-        uint16_t cfg_register = readRegister(INA219_CFG_REG_ADDR) & ~(0b11 << 11);	// Читаем регистр калибровки, сбрасываем биты диапазона напряжения шунта
-        uint16_t vshunt_max_mv = (_r_shunt * _i_max) * 1000;							// Считаем макс напряжение на шунте при макс ожидаемом токе
-
-        if (vshunt_max_mv <= 40) cfg_register |= 0b00 << 11;							// +/- 40mV  - выбираем диапазон
-        else if (vshunt_max_mv <= 80) cfg_register |= 0b01 << 11;						// +/- 80mV
-        else if (vshunt_max_mv <= 160) cfg_register |= 0b10 << 11;					// +/- 160mV
-        else cfg_register |= 0b11 << 11;												// +/- 320mV
-        writeRegister(INA219_CFG_REG_ADDR, cfg_register);								// Пишем конф. регистр
+        if (vshunt_max_mv <= 40) cfg_register |= 0b00 << 11;        // +/- 40mV  - выбираем диапазон
+        else if (vshunt_max_mv <= 80) cfg_register |= 0b01 << 11;   // +/- 80mV
+        else if (vshunt_max_mv <= 160) cfg_register |= 0b10 << 11;  // +/- 160mV
+        else cfg_register |= 0b11 << 11;                            // +/- 320mV
+        writeRegister(INA219_CFG_REG_ADDR, cfg_register);           // Пишем конф. регистр
     }
 };
 #endif
--- src/INA226.h
+++ src/INA226.h
@@ -68,67 +68,78 @@ public:
 
     INA226(const uint8_t address)
     : _r_shunt(0.1f), _i_max(0.8f), _iic_address(address) {}
-
-    bool begin(void) {                      // Инициализация и проверка
-        Wire.begin();                         // Инициализация шины I2c
-        if (!testConnection()) return false;  // Проверка присутствия
-        calibrate();                          // Рассчет калибровочного значения и инициализация
-        return true;                          // Вернуть true если все ок
+    
+    // Инициализация и проверка
+    bool begin(void) {
+        Wire.begin();                           // Инициализация шины I2c
+        if (!testConnection()) return false;    // Проверка присутствия
+        calibrate();                            // расчёт калибровочного значения и инициализация
+        return true;                            // Вернуть true если все ок
     }
-
-    void sleep(bool state) {                                                      // Установка / снятие режима сна
+    
+    // Установка / снятие режима сна
+    void sleep(bool state) {
         uint16_t cfg_register = readRegister(INA226_CFG_REG_ADDR) & ~(0b111);       // Прочитать конф. регистр и стереть биты режима
         writeRegister(INA226_CFG_REG_ADDR, cfg_register | (state ? 0b000 : 0b111)); // Записать новое значение конф. регистра с выбранным режимом
     }
-
-    void adjCalibration(int16_t adj) {          // Подстройка калибровочного значения
-        setCalibration(getCalibration() + adj);   // Читаем и модифицируем значение
-        _cal_value = _cal_value + adj;            // Обновляем внутреннюю переменную
+    
+    // Подстройка калибровочного значения
+    void adjCalibration(int16_t adj) {
+        setCalibration(getCalibration() + adj);     // Читаем и модифицируем значение
+        _cal_value = _cal_value + adj;              // Обновляем внутреннюю переменную
     }
-
-    void setCalibration(uint16_t cal) {         // Установка калибровочного значения
-        writeRegister(INA226_CAL_REG_ADDR, cal);  // Пишем значение в регистр калибровки
-        _cal_value = cal;                         // Обновляем внутреннюю переменную
+    
+    // Установка калибровочного значения
+    void setCalibration(uint16_t cal) {
+        writeRegister(INA226_CAL_REG_ADDR, cal);    // Пишем значение в регистр калибровки
+        _cal_value = cal;                           // Обновляем внутреннюю переменную
     }
-
-    uint16_t getCalibration(void) {                     // Чтение калибровочного значения
-        _cal_value = readRegister(INA226_CAL_REG_ADDR);   // Обновляем внутреннюю переменну
-        return _cal_value;                                // Возвращаем значение
+    
+    // Чтение калибровочного значения
+    uint16_t getCalibration(void) {
+        _cal_value = readRegister(INA226_CAL_REG_ADDR);     // Обновляем внутреннюю переменну
+        return _cal_value;                                  // Возвращаем значение
     }
-
-    void setAveraging(uint8_t avg) {                                               // Установка встроенного усреднения выборок
-        uint16_t cfg_register = readRegister(INA226_CFG_REG_ADDR) & ~(0b111 << 9);   // Читаем конф. регистр, сбросив биты AVG2-0
-        writeRegister(INA226_CFG_REG_ADDR, cfg_register | avg << 9);                 // Пишем новое значение конф. регистр
+    
+    // Установка встроенного усреднения выборок
+    void setAveraging(uint8_t avg) {
+        uint16_t cfg_register = readRegister(INA226_CFG_REG_ADDR) & ~(0b111 << 9);  // Читаем конф. регистр, сбросив биты AVG2-0
+        writeRegister(INA226_CFG_REG_ADDR, cfg_register | avg << 9);                // Пишем новое значение конф. регистр
     }
-
-    void setSampleTime(bool ch, uint8_t mode) {                   // Установка разрешения для выбранного канала
+    
+    // Установка разрешения для выбранного канала
+    void setSampleTime(bool ch, uint8_t mode) {
         uint16_t cfg_register = readRegister(INA226_CFG_REG_ADDR);  // Читаем конф. регистр
         cfg_register &= ~ ((0b111) << (ch ? 6 : 3));                // Сбрасываем нужную пачку бит, в зависимости от канала
         cfg_register |= mode << (ch ? 6 : 3);                       // Пишем нужную пачку бит, в зависимости от канала
         writeRegister(INA226_CFG_REG_ADDR, cfg_register);           // Пишем новое значение конф. регистра
     }
-
-    float getShuntVoltage(void) {                           // Чтение напряжения на шунте
-        setCalibration(_cal_value);                           // Принудительное обновление калибровки (на случай внезапного ребута INA219)
-        int16_t value = readRegister(INA226_SHUNT_REG_ADDR);  // Чтение регистра напряжения шунта
-        return value * 0.000025f;                             // LSB = 2.5uV = 0.000025V, умножаем и возвращаем
+    
+    // Чтение напряжения на шунте
+    float getShuntVoltage(void) {
+        setCalibration(_cal_value);                             // Принудительное обновление калибровки (на случай внезапного ребута INA219)
+        int16_t value = readRegister(INA226_SHUNT_REG_ADDR);    // Чтение регистра напряжения шунта
+        return value * 0.000025f;                               // LSB = 2.5uV = 0.000025V, умножаем и возвращаем
     }
-
-    float getVoltage(void) {                                // Чтение напряжения
-        uint16_t value = readRegister(INA226_VBUS_REG_ADDR);  // Чтение регистра напряжения
-        return value * 0.00125f;                              // LSB = 1.25mV = 0.00125V, Сдвигаем значение до 12 бит и умножаем
+    
+    // Чтение напряжения
+    float getVoltage(void) {
+        uint16_t value = readRegister(INA226_VBUS_REG_ADDR);    // Чтение регистра напряжения
+        return value * 0.00125f;                                // LSB = 1.25mV = 0.00125V, Сдвигаем значение до 12 бит и умножаем
     }
-
-    float getCurrent(void) {                                // Чтение тока
-        setCalibration(_cal_value);                           // Принудительное обновление калибровки (на случай внезапного ребута INA219)
-        int16_t value = readRegister(INA226_CUR_REG_ADDR);    // Чтение регистра тока
-        return value * _current_lsb;                          // LSB рассчитывается на основе макс. ожидаемого тока, умножаем и возвращаем
+    
+    // Чтение тока
+    float getCurrent(void) {
+        setCalibration(_cal_value);                             // Принудительное обновление калибровки (на случай внезапного ребута INA219)
+        int16_t value = readRegister(INA226_CUR_REG_ADDR);      // Чтение регистра тока
+        return value * _current_lsb;                            // LSB рассчитывается на основе макс. ожидаемого тока, умножаем и возвращаем
     }
-
-    float getPower(void) {                                  // Чтение мощности
-        setCalibration(_cal_value);                           // Принудительное обновление калибровки (на случай внезапного ребута INA219)
-        uint16_t value = readRegister(INA226_POWER_REG_ADDR); // Чтение регистра мощности
-        return value * _power_lsb;                            // LSB в 25 раз больше LSB для тока, умножаем возвращаем
+    
+    // Чтение мощности
+    float getPower(void) {
+        setCalibration(_cal_value);                             // Принудительное обновление калибровки (на случай внезапного ребута INA219)
+        uint16_t value = readRegister(INA226_POWER_REG_ADDR);   // Чтение регистра мощности
+        return value * _power_lsb;                              // LSB в 25 раз больше LSB для тока, умножаем возвращаем
     }
 
 private:
@@ -139,36 +150,40 @@ private:
     float _current_lsb = 0.0;           // LSB для тока
     float _power_lsb = 0.0;             // LSB для мощности
     uint16_t _cal_value = 0;            // Калибровочное значение
-
-    void writeRegister(uint8_t address, uint16_t data) {  // Запись 16-ти битного регистра INA219
-        Wire.beginTransmission(_iic_address);               // Начинаем передачу
-        Wire.write(address);                                // Отправляем адрес
-        Wire.write(highByte(data));                         // Отправляем старший байт
-        Wire.write(lowByte(data));                          // Отправляем младший байт
-        Wire.endTransmission();                             // Заканчиваем передачу
+    
+    // Запись 16-ти битного регистра INA219
+    void writeRegister(uint8_t address, uint16_t data) {
+        Wire.beginTransmission(_iic_address);   // Начинаем передачу
+        Wire.write(address);                    // Отправляем адрес
+        Wire.write(highByte(data));             // Отправляем старший байт
+        Wire.write(lowByte(data));              // Отправляем младший байт
+        Wire.endTransmission();                 // Заканчиваем передачу
     }
-
-    uint16_t readRegister(uint8_t address) {  // Чтение 16-ти битного регистра INA219
+    
+    // Чтение 16-ти битного регистра INA219
+    uint16_t readRegister(uint8_t address) {
         Wire.beginTransmission(_iic_address);   // Начинаем передачу
         Wire.write(address);                    // Отправляем адрес
         Wire.endTransmission();                 // Заканчиваем передачу
-        Wire.requestFrom(_iic_address, 2);      // Запрашиваем 2 байта
+        Wire.requestFrom(_iic_address, (uint8_t)2); // Запрашиваем 2 байта
         return Wire.read() << 8 | Wire.read();  // Клеим и возвращаем результат
     }
-
-    bool testConnection(void) {               // Проверка присутствия
+    
+    // Проверка присутствия
+    bool testConnection(void) {
         Wire.beginTransmission(_iic_address);   // Начинаем передачу
         return (bool)!Wire.endTransmission();   // Сразу заканчиваем, инвертируем результат
     }
+    
+    // Процедура расчёта калибровочного значения и инициализации
+    void calibrate(void) {
+        writeRegister(INA226_CFG_REG_ADDR, 0x8000);                 // Принудительный сброс
 
-    void calibrate(void) {                                       // Процедура рассчета калибровочного значения и инициализации
-        writeRegister(INA226_CFG_REG_ADDR, 0x8000);                // Принудительный сброс
+        _current_lsb = _i_max / 32768.0f;                           // расчёт LSB для тока (см. доку INA219)
+        _power_lsb = _current_lsb * 25.0f;                          // расчёт LSB для мощности (см. доку INA219)
+        _cal_value = trunc(0.00512f / (_current_lsb * _r_shunt));   // расчёт калибровочного значения (см. доку INA219)
 
-        _current_lsb = _i_max / 32768.0f;                          // Рассчет LSB для тока (см. доку INA219)
-        _power_lsb = _current_lsb * 25.0f;                         // Рассчет LSB для мощности (см. доку INA219)
-        _cal_value = trunc(0.00512f / (_current_lsb * _r_shunt));  // Рассчет калибровочного значения (см. доку INA219)
-
-        setCalibration(_cal_value);                                // Записываем стандартное калибровочное значение
+        setCalibration(_cal_value);                                 // Записываем стандартное калибровочное значение
     }
 };
 #endif
